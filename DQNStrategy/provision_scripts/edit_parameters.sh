84ecb59461eb6e00f79f026c8825980bc51e47ee
samanvayms03 <samanvayms03@icloud.com>
2024-03-28 23:36:36 -0500

    debug

--- DQNStrategy/provision_scripts/edit_parameters.sh
+++ DQNStrategy/provision_scripts/edit_parameters.sh
@@ -2,12 +2,12 @@ instanceName="$1"
 strategyName="$2"
 new_params="$3"
 
-echo "changing parameters to $new_params" &&
-cd /home/vagrant/ss/bt/utilities && ./StrategyCommandLine cmd params $new_params {-name $instanceName} {-type $strategyName} &&
+echo "changing parameters to $new_params" 
+cd /home/vagrant/ss/bt/utilities && ./StrategyCommandLine cmd params $new_params {-name $instanceName} {-type $strategyName}
 
-echo "parameters changed resetting strategy" &&
-cd /home/vagrant/ss/bt/utilities && ./StrategyCommandLine cmd recheck_strategies &&
+echo "parameters changed resetting strategy" 
+cd /home/vagrant/ss/bt/utilities && ./StrategyCommandLine cmd recheck_strategies 
 
-echo "strategy reset succesfully" &&
+echo "strategy reset succesfully" 
 echo " **************************************************************************************************** "
 
--- DQNStrategy/provision_scripts/start_server.sh
+++ DQNStrategy/provision_scripts/start_server.sh
@@ -1,9 +1,9 @@
 
-echo "Starting server" &&
+echo "Starting server" 
 # quits previously created server
 cd /home/vagrant/ss/bt/utilities && ./StrategyCommandLine cmd quit
 # starts up the server and keeps it in the background
 cd /home/vagrant/ss/bt/ && ./StrategyServerBacktesting server.log 2>&1 & 
-echo "Started server" &&
+echo "Started server" 
 echo " **************************************************************************************************** "
 
