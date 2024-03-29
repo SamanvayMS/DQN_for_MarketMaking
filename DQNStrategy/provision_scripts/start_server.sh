b705d3d1935b393d7e46e74c78bfc88d2e5a8570
samanvayms03 <samanvayms03@icloud.com>
2024-03-28 23:34:20 -0500

    debug again

--- DQNStrategy/provision_scripts/start_server.sh
+++ DQNStrategy/provision_scripts/start_server.sh
@@ -3,7 +3,7 @@ echo "Starting server" &&
 # quits previously created server
 cd /home/vagrant/ss/bt/utilities && ./StrategyCommandLine cmd quit
 # starts up the server and keeps it in the background
-cd /home/vagrant/ss/bt/ && ./StrategyServerBacktesting server.log 2>&1 & &&
+cd /home/vagrant/ss/bt/ && ./StrategyServerBacktesting server.log 2>&1 & 
 echo "Started server" &&
 echo " **************************************************************************************************** "
 
