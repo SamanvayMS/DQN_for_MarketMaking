54df47a84134b9a8560260ee84c03596b0f638b6
samanvayms03 <samanvayms03@icloud.com>
2024-03-29 01:35:13 -0500

    chaining did not work

--- DQNStrategy/provision_scripts/start_server.sh
+++ DQNStrategy/provision_scripts/start_server.sh
@@ -1,7 +1,7 @@
 
 echo "Starting server" 
 # quits previously created server
-cd /home/vagrant/ss/bt/utilities && ./StrategyCommandLine cmd quit &&
+cd /home/vagrant/ss/bt/utilities && ./StrategyCommandLine cmd quit 
 # starts up the server and keeps it in the background
 cd /home/vagrant/ss/bt/ && ./StrategyServerBacktesting server.log 2>&1 & 
 sleep 10 # sleep for 10 seconds
